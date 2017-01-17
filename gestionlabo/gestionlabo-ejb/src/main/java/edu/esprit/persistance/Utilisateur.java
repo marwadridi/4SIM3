5b00a4fd88c573f579ca4497911952771ae49bbf
marwa <marwa.dridi@esprit.tn>
2017-01-17 09:21:41 +0100

    archetype maven (jpa entity/ persisitance.xml config

---
+++ gestionlabo/gestionlabo-ejb/src/main/java/edu/esprit/persistance/Utilisateur.java
@@ -0,0 +1,62 @@
+package edu.esprit.persistance;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_user")
+public class Utilisateur implements Serializable{
+
+	/**
+	 * 
+	 */
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.TABLE)
+	private int id;
+	
+	@Column(name="c_nom")
+	private String nom;
+	private String login;
+	private String password;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
